39064db12cd9069dd248a349469b0b54716388ce
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-29 12:14:07 +0530

    Update main.go

--- main.go
+++ main.go
@@ -26,7 +26,6 @@ var movies []Movie
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(movies)
-
 }
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
